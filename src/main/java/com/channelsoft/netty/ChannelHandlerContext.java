a087ae3c7b0f33a6ca7e44a531337993e9a2c261
sichengwen <1224260992@qq.com>
2019-08-18 13:09:42 +0800

    fix

--- src/main/java/com/channelsoft/netty/ChannelHandlerContext.java
+++ src/main/java/com/channelsoft/netty/ChannelHandlerContext.java
@@ -7,8 +7,15 @@ public class ChannelHandlerContext {
 
     private ChannelHandler channelHandler;
 
+    private boolean inbound;
+    private boolean outbound;
+
     private ChannelHandlerContext findContextInbound(){
-        return null;
+        ChannelHandlerContext ctx = this;
+        do {
+            ctx = ctx.next;
+        } while (!ctx.inbound);
+        return ctx;
     }
 
     private ChannelHandlerContext findContextOutbound(){
@@ -26,12 +33,43 @@ public class ChannelHandlerContext {
         inboundHandler.channelRead(this,msg);
     }
 
+    public ChannelHandlerContext getPrev() {
+        return prev;
+    }
 
-    /**
-     * 自己的handler处理
-     * write
-     * read
-     *
-     * firexxx是将请求下发到后面的handler
-     */
+    public void setPrev(ChannelHandlerContext prev) {
+        this.prev = prev;
+    }
+
+    public ChannelHandlerContext getNext() {
+        return next;
+    }
+
+    public void setNext(ChannelHandlerContext next) {
+        this.next = next;
+    }
+
+    public ChannelHandler getChannelHandler() {
+        return channelHandler;
+    }
+
+    public void setChannelHandler(ChannelHandler channelHandler) {
+        this.channelHandler = channelHandler;
+    }
+
+    public boolean isInbound() {
+        return inbound;
+    }
+
+    public void setInbound(boolean inbound) {
+        this.inbound = inbound;
+    }
+
+    public boolean isOutbound() {
+        return outbound;
+    }
+
+    public void setOutbound(boolean outbound) {
+        this.outbound = outbound;
+    }
 }
